10459d266df7145cfd0365c29cc323c7dc3ca580
MitchellHurry <56243702+MitchellHurry@users.noreply.github.com>
2022-10-05 19:31:57 +1100

    Hurry Home API Call - No Loop

--- simpleAPI.go
+++ simpleAPI.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Prinln("Hello World")
+	//https://levelup.gitconnected.com/consuming-a-rest-api-using-golang-b323602ba9d8
+	//fmt.Println("Calling API...")
+
+	client := &http.Client{}
+	apiKey := "5aRdCP324H39P1290TR4K4PxnayTE8G19C5jiAg9"
+	req, err := http.NewRequest("POST", "https://7nbx4tfpk5.execute-api.ap-southeast-2.amazonaws.com/prod", nil)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("x-api-key", apiKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	defer resp.Body.Close()
 }
